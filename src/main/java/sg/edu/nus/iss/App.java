22214a41f2b682e7db0dd6eba43249938b70aefe
nigeleee <nigeleee@gmail.com>
2023-04-18 15:38:13 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -0,0 +1,24 @@
+package sg.edu.nus.iss;
+
+/**
+ * Hello world!
+ *
+ */
+public class BankAccount {
+
+public class BankAccount {
+    private final String fullName;
+    private final String accoutnNumber;
+    private float balance;
+    private boolean isClosed;
+    private Date accountStartDate;
+    private Date accountEndDate;
+    private 
+}    
+}
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
